6c21c3c450bbae768375f9b0c634f46424390e6e
David Daney <david.daney@cavium.com>
2012-07-05 18:12:38 +0200

    MIPS: Prune some target specific code out of prom.c
    This code is not common enough to be in a shared file.  It is also not
    used by any existing boards, so just remove it.
    
    [ralf@linux-mips.org: Dropped removal of irq_create_of_mapping which was
    already removed by abd2363f6a5f1030b935e0bdc15cf917313b3b10
    [irq_domain/mips: Allow irq_domain on MIPS].  Moved device_tree_init() and
    dependencies to its sole user, the XLP code.]
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Cc: linux-mips@linux-mips.org
    Cc: devicetree-discuss@lists.ozlabs.org
    Cc: grant.likely@secretlab.ca
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/2946/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/asm/prom.h
+++ arch/mips/include/asm/prom.h
@@ -17,9 +17,6 @@
 extern int early_init_dt_scan_memory_arch(unsigned long node,
 	const char *uname, int depth, void *data);
 
-extern int reserve_mem_mach(unsigned long addr, unsigned long size);
-extern void free_mem_mach(unsigned long addr, unsigned long size);
-
 extern void device_tree_init(void);
 
 struct boot_param_header;
