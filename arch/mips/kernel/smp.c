a83994a8330fe9c706462860450e801d60233458
Jayachandran C <jchandra@broadcom.com>
2012-08-14 18:56:13 +0530

    MIPS: Synchronize MIPS count one CPU at a time
    The current implementation of synchronise_count_{master,slave} blocks
    slave CPUs in early boot until all of them come up. This no longer
    works because blocking a CPU with interrupts off after notifying the
    CPU to be online causes problems with the current kernel.
    
    Specifically, after the workqueue changes
    (commit a08489c569dc1 "Pull workqueue changes from Tejun Heo")
    the CPU_ONLINE notification callback workqueue_cpu_up_callback()
    will hang on wait_for_completion(&idle_rebind.done), if the slave
    CPUs are blocked for synchronize_count_slave().
    
    The changes are to update synchronize_count_{master,slave}() to handle
    one CPU at a time and to call synchronise_count_master() in __cpu_up()
    so that the CPU_ONLINE notification goes out only after the COP0 COUNT
    register is synchronized.
    
    [ralf@linux-mips.org: This matter only to those few platforms which are
    using the cp0 counter as their clocksource which are XLP, XLR and MIPS'
    CMP solution.]
    
    Signed-off-by: Jayachandran C <jchandra@broadcom.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/4216/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/kernel/smp.c
+++ arch/mips/kernel/smp.c
@@ -152,7 +152,7 @@ asmlinkage __cpuinit void start_secondary(void)
 
 	cpu_set(cpu, cpu_callin_map);
 
-	synchronise_count_slave();
+	synchronise_count_slave(cpu);
 
 	/*
 	 * irq will be enabled in ->smp_finish(), enabling it too early
@@ -195,7 +195,6 @@ void smp_send_stop(void)
 void __init smp_cpus_done(unsigned int max_cpus)
 {
 	mp_ops->cpus_done();
-	synchronise_count_master();
 }
 
 /* called from main before smp_init() */
@@ -229,6 +228,7 @@ int __cpuinit __cpu_up(unsigned int cpu, struct task_struct *tidle)
 	while (!cpu_isset(cpu, cpu_callin_map))
 		udelay(100);
 
+	synchronise_count_master(cpu);
 	return 0;
 }
 
