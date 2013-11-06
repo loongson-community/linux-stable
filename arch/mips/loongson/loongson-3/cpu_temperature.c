d35b41c52828cb36a75ff19278a8830e9267cad3
Huacai Chen <chenhc@lemote.com>
2013-11-06 15:19:05 +0800

    MIPS: Loongson 3: Add platform device and driver for new machines
    New machines include A1214/A1215/A1217/A1219.
    EMC1412 and TMP75 are temperature sensors, LM93 control fans.
    
    Signed-off-by: Hongbing Hu <huhb@lemote.com>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/loongson/loongson-3/cpu_temperature.c
+++ arch/mips/loongson/loongson-3/cpu_temperature.c
@@ -1,9 +1,11 @@
 #include <linux/types.h>
+#include <loongson_hwmon.h>
 
 #ifdef CONFIG_64BIT
-#define TEMPRATURE_SENSOR_REG 0xffffffffbfe0019c
+#define CPU0_TEMPRATURE_SENSOR_REG 0x900000001fe0019c
+#define CPU1_TEMPRATURE_SENSOR_REG 0x900010001fe0019c
 #else
-#define TEMPRATURE_SENSOR_REG 0xbfe0019c
+#define CPU0_TEMPRATURE_SENSOR_REG 0xbfe0019c
 #endif
 
 /*
@@ -12,11 +14,23 @@
  * if more than 127, that is dangerous.
  * here only provide sensor1 data, because it always hot than sensor0
  */
-int loongson3_cpu_temp(void)
+int loongson3_cpu_temp(int id)
 {
 	u32 reg;
+	void *addr;
 
-	reg = *(volatile u32 *)TEMPRATURE_SENSOR_REG;
+	switch (id) {
+	case 1:
+		addr = (void *)CPU0_TEMPRATURE_SENSOR_REG;
+		break;
+	case 6:
+		addr = (void *)CPU1_TEMPRATURE_SENSOR_REG;
+		break;
+	default:
+		return NOT_VALID_TEMP;
+	}
+
+	reg = *(volatile u32 *)addr;
 	reg = (reg >> 8) & 0xff;
 
 	return (int)reg * 1000;
