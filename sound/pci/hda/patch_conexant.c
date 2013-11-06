370d25123c55c70b0bb1c019a94a090e2baa955d
Xiang Yu <xiangy@lemote.com>
2013-11-06 15:19:08 +0800

    add rear mic support for CX20631
    CX20631 Node1b's default config value should be 0x01A190F0 according to CX20631
    datesheet, but the true value is 0x018xxxxx, this cause PortC can't work as rear
    mic, so rewrite to default value

--- sound/pci/hda/patch_conexant.c
+++ sound/pci/hda/patch_conexant.c
@@ -1058,6 +1058,9 @@ static int patch_conexant_auto(struct hda_codec *codec)
 		snd_hda_pick_fixup(codec, cxt5051_fixup_models,
 				   cxt5051_fixups, cxt_fixups);
 		break;
+	/* CX20631 node1b's default value is not same as datasheet, cause rear mic not work */
+	case 0x14f15097:
+		snd_hda_codec_set_pincfg(codec, 0x1b, 0x01a190f0);
 	default:
 		codec->pin_amp_workaround = 1;
 		snd_hda_pick_fixup(codec, cxt5066_fixup_models,
