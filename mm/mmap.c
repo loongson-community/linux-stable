db72a8bfc27997899efb9b4a6ee0b7ce0522c7ec
Akira Takeuchi <takeuchi.akr@jp.panasonic.com>
2013-11-12 15:08:21 -0800

    mm: ensure get_unmapped_area() returns higher address than mmap_min_addr
    This patch fixes the problem that get_unmapped_area() can return illegal
    address and result in failing mmap(2) etc.
    
    In case that the address higher than PAGE_SIZE is set to
    /proc/sys/vm/mmap_min_addr, the address lower than mmap_min_addr can be
    returned by get_unmapped_area(), even if you do not pass any virtual
    address hint (i.e.  the second argument).
    
    This is because the current get_unmapped_area() code does not take into
    account mmap_min_addr.
    
    This leads to two actual problems as follows:
    
    1. mmap(2) can fail with EPERM on the process without CAP_SYS_RAWIO,
       although any illegal parameter is not passed.
    
    2. The bottom-up search path after the top-down search might not work in
       arch_get_unmapped_area_topdown().
    
    Note: The first and third chunk of my patch, which changes "len" check,
    are for more precise check using mmap_min_addr, and not for solving the
    above problem.
    
    [How to reproduce]
    
    	--- test.c -------------------------------------------------
    	#include <stdio.h>
    	#include <unistd.h>
    	#include <sys/mman.h>
    	#include <sys/errno.h>
    
    	int main(int argc, char *argv[])
    	{
    		void *ret = NULL, *last_map;
    		size_t pagesize = sysconf(_SC_PAGESIZE);
    
    		do {
    			last_map = ret;
    			ret = mmap(0, pagesize, PROT_NONE,
    				MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
    	//		printf("ret=%p\n", ret);
    		} while (ret != MAP_FAILED);
    
    		if (errno != ENOMEM) {
    			printf("ERR: unexpected errno: %d (last map=%p)\n",
    			errno, last_map);
    		}
    
    		return 0;
    	}
    	---------------------------------------------------------------
    
    	$ gcc -m32 -o test test.c
    	$ sudo sysctl -w vm.mmap_min_addr=65536
    	vm.mmap_min_addr = 65536
    	$ ./test  (run as non-priviledge user)
    	ERR: unexpected errno: 1 (last map=0x10000)
    
    Signed-off-by: Akira Takeuchi <takeuchi.akr@jp.panasonic.com>
    Signed-off-by: Kiyoshi Owada <owada.kiyoshi@jp.panasonic.com>
    Reviewed-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1397,7 +1397,7 @@ arch_get_unmapped_area(struct file *filp, unsigned long addr,
 	struct vm_area_struct *vma;
 	unsigned long start_addr;
 
-	if (len > TASK_SIZE)
+	if (len > TASK_SIZE - mmap_min_addr)
 		return -ENOMEM;
 
 	if (flags & MAP_FIXED)
@@ -1406,7 +1406,7 @@ arch_get_unmapped_area(struct file *filp, unsigned long addr,
 	if (addr) {
 		addr = PAGE_ALIGN(addr);
 		vma = find_vma(mm, addr);
-		if (TASK_SIZE - len >= addr &&
+		if (TASK_SIZE - len >= addr && addr >= mmap_min_addr &&
 		    (!vma || addr + len <= vma->vm_start))
 			return addr;
 	}
@@ -1469,9 +1469,10 @@ arch_get_unmapped_area_topdown(struct file *filp, const unsigned long addr0,
 	struct vm_area_struct *vma;
 	struct mm_struct *mm = current->mm;
 	unsigned long addr = addr0, start_addr;
+	unsigned long low_limit = max(PAGE_SIZE, mmap_min_addr);
 
 	/* requested length too big for entire address space */
-	if (len > TASK_SIZE)
+	if (len > TASK_SIZE - mmap_min_addr)
 		return -ENOMEM;
 
 	if (flags & MAP_FIXED)
@@ -1481,7 +1482,7 @@ arch_get_unmapped_area_topdown(struct file *filp, const unsigned long addr0,
 	if (addr) {
 		addr = PAGE_ALIGN(addr);
 		vma = find_vma(mm, addr);
-		if (TASK_SIZE - len >= addr &&
+		if (TASK_SIZE - len >= addr && addr >= mmap_min_addr &&
 				(!vma || addr + len <= vma->vm_start))
 			return addr;
 	}
@@ -1496,7 +1497,7 @@ try_again:
 	/* either no address requested or can't fit in requested address hole */
 	start_addr = addr = mm->free_area_cache;
 
-	if (addr < len)
+	if (addr < low_limit + len)
 		goto fail;
 
 	addr -= len;
@@ -1517,7 +1518,7 @@ try_again:
 
 		/* try just below the current vma->vm_start */
 		addr = vma->vm_start-len;
-	} while (len < vma->vm_start);
+	} while (vma->vm_start >= low_limit + len);
 
 fail:
 	/*
