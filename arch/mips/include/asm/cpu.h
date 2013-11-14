1e113c2eced77b8fbc1a632426e0332389bf0b9b
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2013-11-14 16:12:26 +0000

    MIPS: Add processor identifiers for the proAptiv processors
    Add processor identifiers for single core and multi-core
    proAptiv processors.
    
    Signed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/6133/
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/asm/cpu.h
+++ arch/mips/include/asm/cpu.h
@@ -94,6 +94,8 @@
 #define PRID_IMP_24KE		0x9600
 #define PRID_IMP_74K		0x9700
 #define PRID_IMP_1004K		0x9900
+#define PRID_IMP_PROAPTIV_UP	0xa200
+#define PRID_IMP_PROAPTIV_MP	0xa300
 
 /*
  * These are the PRID's for when 23:16 == PRID_COMP_SIBYTE
