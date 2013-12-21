841fc089b2b17393f1fd56d784b9ea4baa4ea172
Huacai Chen <chenhc@lemote.com>
2013-12-21 10:20:45 +0800

    MIPS: Support hard limit of cpu count (nr_cpu_ids)
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/kernel/setup.c
+++ arch/mips/kernel/setup.c
@@ -720,6 +720,23 @@ static void __init resource_init(void)
 	}
 }
 
+static void __init prefill_possible_map(void)
+{
+#ifdef CONFIG_SMP
+	int i, possible = num_possible_cpus();
+
+	if (possible > nr_cpu_ids)
+		possible = nr_cpu_ids;
+
+	for (i = 0; i < possible; i++)
+		set_cpu_possible(i, true);
+	for (; i < NR_CPUS; i++)
+		set_cpu_possible(i, false);
+
+	nr_cpu_ids = possible;
+#endif
+}
+
 void __init setup_arch(char **cmdline_p)
 {
 	cpu_probe();
@@ -743,6 +760,7 @@ void __init setup_arch(char **cmdline_p)
 
 	resource_init();
 	plat_smp_setup();
+	prefill_possible_map();
 
 	cpu_cache_init();
 }
