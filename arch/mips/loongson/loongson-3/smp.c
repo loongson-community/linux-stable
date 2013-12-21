c0dad0e7229af18f6b741648f84d2c4a03ae1ec1
Huacai Chen <chenhc@lemote.com>
2013-12-21 10:20:50 +0800

    MIPS: Loongson 3: Introduce disable_unused_cpus() to save power.
    When use maxcpus=n to limit the number of cores, the initial offlined
    cores are running a infinite loop in firmware and their clocks are not
    disabled. For power saving, we take the unused cores up and then down
    at boot time. When resume from STR, we do this the same.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Hongliang Tao <taohl@lemote.com>
    Signed-off-by: Hua Yan <yanh@lemote.com>

--- arch/mips/loongson/loongson-3/smp.c
+++ arch/mips/loongson/loongson-3/smp.c
@@ -324,6 +324,7 @@ void __cpuinit loongson3_boot_secondary(int cpu, struct task_struct *idle)
  */
 void __init loongson3_cpus_done(void)
 {
+	disable_unused_cpus();
 }
 
 #ifdef CONFIG_HOTPLUG_CPU
@@ -580,6 +581,21 @@ static int __cpuinit register_loongson3_notifier(void)
 	return 0;
 }
 early_initcall(register_loongson3_notifier);
+
+void __cpuinit disable_unused_cpus(void)
+{
+	int cpu;
+	struct cpumask tmp;
+
+	cpumask_complement(&tmp, cpu_online_mask);
+	cpumask_and(&tmp, &tmp, cpu_possible_mask);
+
+	for_each_cpu(cpu, &tmp)
+		cpu_up(cpu);
+
+	for_each_cpu(cpu, &tmp)
+		cpu_down(cpu);
+}
 
 #endif
 
