79b9d9b93c872592f2d0537cc8768b11da469512
Huacai Chen <chenhc@lemote.com>
2014-03-25 20:12:44 +0800

    MIPS: Loongson-3: Enable the COP2 usage
    Loongson-3 has some specific instructions (MMI/SIMD) in coprocessor 2.
    COP2 isn't independent because it share COP1 (FPU)'s registers. This
    patch enable the COP2 usage so user-space programs can use the MMI/SIMD
    instructions. When COP2 exception happens, we enable both COP1 (FPU)
    and COP2, only in this way the fp context can be saved and restored
    correctly.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

---
+++ arch/mips/loongson/loongson-3/cop2-ex.c
@@ -0,0 +1,55 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2014 Lemote Corporation.
+ *   written by Huacai Chen <chenhc@lemote.com>
+ *
+ * based on arch/mips/cavium-octeon/cpu.c
+ * Copyright (C) 2009 Wind River Systems,
+ *   written by Ralf Baechle <ralf@linux-mips.org>
+ */
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/notifier.h>
+
+#include <asm/fpu.h>
+#include <asm/cop2.h>
+#include <asm/current.h>
+#include <asm/mipsregs.h>
+
+static int loongson_cu2_call(struct notifier_block *nfb, unsigned long action,
+	void *data)
+{
+	int fpu_owned;
+
+	switch (action) {
+	case CU2_EXCEPTION:
+		preempt_disable();
+		fpu_owned = __is_fpu_owner();
+		set_c0_status(ST0_CU1 | ST0_CU2);
+		enable_fpu_hazard();
+		KSTK_STATUS(current) |= (ST0_CU1 | ST0_CU2);
+		/* If FPU is owned, we needn't init or restore fp */
+		if(!fpu_owned) {
+			set_thread_flag(TIF_USEDFPU);
+			if (!used_math()) {
+				_init_fpu();
+				set_used_math();
+			} else
+				_restore_fp(current);
+		}
+		preempt_enable();
+
+		return NOTIFY_STOP;	/* Don't call default notifier */
+	}
+
+	return NOTIFY_OK;		/* Let default notifier send signals */
+}
+
+static int __init loongson_cu2_setup(void)
+{
+	return cu2_notifier(loongson_cu2_call, 0);
+}
+early_initcall(loongson_cu2_setup);
