01cefd20ece7ee4496e3ea1b22da47fcd5b024ba
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2014-07-15 14:09:56 +0100

    MIPS: Use dedicated exception handler if CPU supports RI/XI exceptions
    Use the regular tlb_do_page_fault_0 (no write) handler to handle
    the RI and XI exceptions. Also skip the RI/XI validation check
    on TLB load handler since it's redundant when the CPU has
    unique RI/XI exceptions.
    
    Singed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/7339/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -83,6 +83,7 @@ extern asmlinkage void handle_mt(void);
 extern asmlinkage void handle_dsp(void);
 extern asmlinkage void handle_mcheck(void);
 extern asmlinkage void handle_reserved(void);
+extern void tlb_do_page_fault_0(void);
 
 void (*board_be_init)(void);
 int (*board_be_handler)(struct pt_regs *regs, int is_fixup);
@@ -1964,6 +1965,12 @@ void __init trap_init(void)
 		set_except_vector(15, handle_fpe);
 
 	set_except_vector(16, handle_ftlb);
+
+	if (cpu_has_rixiex) {
+		set_except_vector(19, tlb_do_page_fault_0);
+		set_except_vector(20, tlb_do_page_fault_0);
+	}
+
 	set_except_vector(22, handle_mdmx);
 
 	if (cpu_has_mcheck)
