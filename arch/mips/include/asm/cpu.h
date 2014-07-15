6c576152bf00bd3f5647219687f784982a80bce4
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2014-07-15 14:09:55 +0100

    MIPS: Add new option for unique RI/XI exceptions
    MIPSr5 added support for unique exception codes for the Read-Inhibit
    and Execute-Inhibit exceptions.
    
    Signed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/7338/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/asm/cpu.h
+++ arch/mips/include/asm/cpu.h
@@ -326,6 +326,7 @@ enum cpu_type_enum {
 #define MIPS_CPU_RIXI		0x00800000ull /* CPU has TLB Read/eXec Inhibit */
 #define MIPS_CPU_TLBINV		0x01000000ull /* CPU supports TLBINV/F */
 #define MIPS_CPU_HTW		0x020000000ull /* CPU support Hardware Page Table Walker */
+#define MIPS_CPU_RIXIEX		0x040000000ull /* CPU has unique exception codes for {Read, Execute}-Inhibit exceptions */
 
 /*
  * CPU ASE encodings
