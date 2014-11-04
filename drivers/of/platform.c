fd29e17d8dd95b16bb5075dbc91171189a9f254d
Grant Likely <grant.likely@linaro.org>
2014-11-04 10:26:26 +0000

    of/platform: Move platform devices under /sys/devices/platform
    Currently the devices created by drivers/of/platform.c get created at
    the root of /sys/devices. This goes against the typical pattern for
    sysfs where the top level /sys/devices structure contains categories of
    devices, and the structure of devices is placed below that. To fix this,
    make the code in drivers/of/platform.c follow the drivers/base/platform.c
    behaviour, and use &platform_bus as the default parent for all new
    platform_devices and amba_devices.
    
    This change has been discussed for a long time, but nobody has actually
    acted on it. Userspace code that expects to find devices under a fixed
    /sys/devices/... path will be affected. It isn't /supposed/ to do that,
    but if anyone complains then I'll add a default-off workaround option to
    put them back into the root.
    
    Signed-off-by: Grant Likely <grant.likely@linaro.org>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -165,7 +165,7 @@ struct platform_device *of_device_alloc(struct device_node *np,
 #if defined(CONFIG_MICROBLAZE)
 	dev->dev.dma_mask = &dev->archdata.dma_mask;
 #endif
-	dev->dev.parent = parent;
+	dev->dev.parent = parent ? : &platform_bus;
 
 	if (bus_id)
 		dev_set_name(&dev->dev, "%s", bus_id);
@@ -262,7 +262,7 @@ static struct amba_device *of_amba_device_create(struct device_node *node,
 	/* setup generic device info */
 	dev->dev.coherent_dma_mask = ~0;
 	dev->dev.of_node = of_node_get(node);
-	dev->dev.parent = parent;
+	dev->dev.parent = parent ? : &platform_bus;
 	dev->dev.platform_data = platform_data;
 	if (bus_id)
 		dev_set_name(&dev->dev, "%s", bus_id);
