ce2bca51b2fde7cf74e9806cdf3c06cbc8ecab2f
Markos Chandras <markos.chandras@imgtec.com>
2014-11-17 09:31:07 +0000

    MIPS: tlb-r4k: Add missing HTW stop/start sequences
    HTW needs to stop and start again whenever the EntryHI register
    changes otherwise an inflight HTW operation might use the new
    EntryHI register for updating an old entry and that could lead
    to crashes or even a machine check exception. We fix this by
    ensuring the HTW has stop whenever the EntryHI register is about
    to change
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: <stable@vger.kernel.org> # v3.17+
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/8511/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/mm/tlb-r4k.c
+++ arch/mips/mm/tlb-r4k.c
@@ -299,6 +299,7 @@ void __update_tlb(struct vm_area_struct * vma, unsigned long address, pte_t pte)
 
 	local_irq_save(flags);
 
+	htw_stop();
 	pid = read_c0_entryhi() & ASID_MASK;
 	address &= (PAGE_MASK << 1);
 	write_c0_entryhi(address | pid);
@@ -346,6 +347,7 @@ void __update_tlb(struct vm_area_struct * vma, unsigned long address, pte_t pte)
 			tlb_write_indexed();
 	}
 	tlbw_use_hazard();
+	htw_start();
 	flush_itlb_vm(vma);
 	local_irq_restore(flags);
 }
