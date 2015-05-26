e25bcb660038006169db6a518853fa04cda510d0
Huacai Chen <chenhc@lemote.com>
2015-05-26 15:14:02 +0800

    MIPS: Loongson-3: Fast TLB refill handler
    Loongson-3A R2 has pwbase/pwfield/pwsize/pwctl registers in CP0 (this
    is very similar to HTW) and lwdir/lwpte/lddir/ldpte instructions which
    can be used for fast TLB refill.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/asm/cpu.h
+++ arch/mips/include/asm/cpu.h
@@ -382,6 +382,7 @@ enum cpu_type_enum {
 #define MIPS_CPU_XPA		0x2000000000ull /* CPU supports Extended Physical Addressing */
 #define MIPS_CPU_CDMM		0x4000000000ull	/* CPU has Common Device Memory Map */
 #define MIPS_CPU_FTLB		0x8000000000ull /* CPU has Fixed-page-size TLB */
+#define MIPS_CPU_LDPTE		0x10000000000ull /* CPU has ldpte/lddir instructions */
 
 /*
  * CPU ASE encodings
