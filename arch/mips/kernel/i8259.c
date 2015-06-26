a79fcd76eabab6c2574dea24cd620e357d3f1b7b
Huacai Chen <chenhc@lemote.com>
2015-06-26 15:40:09 +0800

    Loongson-3: support irq_set_affinity() in i8259 chip
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/kernel/i8259.c
+++ arch/mips/kernel/i8259.c
@@ -44,9 +44,9 @@ static struct irq_chip i8259A_chip = {
 	.irq_disable		= disable_8259A_irq,
 	.irq_unmask		= enable_8259A_irq,
 	.irq_mask_ack		= mask_and_ack_8259A,
-#ifdef CONFIG_MIPS_MT_SMTC_IRQAFF
+#if defined(CONFIG_MIPS_MT_SMTC_IRQAFF) || defined(CONFIG_CPU_LOONGSON3)
 	.irq_set_affinity	= plat_set_irq_affinity,
-#endif /* CONFIG_MIPS_MT_SMTC_IRQAFF */
+#endif
 };
 
 /*
