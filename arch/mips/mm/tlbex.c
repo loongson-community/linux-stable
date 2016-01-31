4239213f893f93f5b74901d5cf61e3efb9b7608f
Huacai Chen <chenhc@lemote.com>
2016-01-31 17:01:58 +0800

    MIPS: Loongson: Introduce and use LOONGSON_LLSC_WAR
    On the Loongson-2G/2H/3A/3B there is a bug that ll / sc and lld / scd
    is very weak ordering. We should add sync instruction to workaround.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/mm/tlbex.c
+++ arch/mips/mm/tlbex.c
@@ -83,6 +83,11 @@ static inline int __maybe_unused r10000_llsc_war(void)
 	return R10000_LLSC_WAR;
 }
 
+static inline int __maybe_unused loongson_llsc_war(void)
+{
+       return LOONGSON_LLSC_WAR;
+}
+
 static int use_bbit_insns(void)
 {
 	switch (current_cpu_type()) {
@@ -295,6 +300,12 @@ static inline void dump_handler(const char *symbol, const u32 *handler, int coun
 #else
 # define GET_CONTEXT(buf, reg) UASM_i_MFC0(buf, reg, C0_CONTEXT)
 #endif
+
+void uasm_i_sync(u32 **buf)
+{
+	**buf = 0xf;
+	(*buf)++;
+}
 
 /* The worst case length of the handler is around 18 instructions for
  * R3000-style TLBs and up to 63 instructions for R4000-style TLBs.
@@ -942,6 +953,8 @@ build_get_pgd_vmalloc64(u32 **p, struct uasm_label **l, struct uasm_reloc **r,
 		 * to mimic that here by taking a load/istream page
 		 * fault.
 		 */
+		if (loongson_llsc_war())
+			uasm_i_sync(p);
 		UASM_i_LA(p, ptr, (unsigned long)tlb_do_page_fault_0);
 		uasm_i_jr(p, ptr);
 
@@ -1548,6 +1561,8 @@ static void __cpuinit build_loongson3_tlb_refill_handler(void)
 
 	if(check_for_high_segbits){
 		uasm_l_large_segbits_fault(&l, p);
+		if (loongson_llsc_war())
+			uasm_i_sync(&p);
 		UASM_i_LA(&p, K1, (unsigned long)tlb_do_page_fault_0);
 		uasm_i_jr(&p, K1);
 		uasm_i_nop(&p);
@@ -1645,6 +1660,8 @@ static void __cpuinit
 iPTE_LW(u32 **p, unsigned int pte, unsigned int ptr)
 {
 #ifdef CONFIG_SMP
+	if (loongson_llsc_war())
+		uasm_i_sync(p);
 # ifdef CONFIG_64BIT_PHYS_ADDR
 	if (cpu_has_64bits)
 		uasm_i_lld(p, pte, 0, ptr);
@@ -2192,6 +2209,8 @@ static void __cpuinit build_r4000_tlb_load_handler(void)
 #endif
 
 	uasm_l_nopage_tlbl(&l, p);
+	if (loongson_llsc_war())
+		uasm_i_sync(&p);
 	build_restore_work_registers(&p);
 	uasm_i_j(&p, (unsigned long)tlb_do_page_fault_0 & 0x0fffffff);
 	uasm_i_nop(&p);
@@ -2240,6 +2259,8 @@ static void __cpuinit build_r4000_tlb_store_handler(void)
 #endif
 
 	uasm_l_nopage_tlbs(&l, p);
+	if (loongson_llsc_war())
+		uasm_i_sync(&p);
 	build_restore_work_registers(&p);
 	uasm_i_j(&p, (unsigned long)tlb_do_page_fault_1 & 0x0fffffff);
 	uasm_i_nop(&p);
@@ -2289,6 +2310,8 @@ static void __cpuinit build_r4000_tlb_modify_handler(void)
 #endif
 
 	uasm_l_nopage_tlbm(&l, p);
+	if (loongson_llsc_war())
+		uasm_i_sync(&p);
 	build_restore_work_registers(&p);
 	uasm_i_j(&p, (unsigned long)tlb_do_page_fault_1 & 0x0fffffff);
 	uasm_i_nop(&p);
