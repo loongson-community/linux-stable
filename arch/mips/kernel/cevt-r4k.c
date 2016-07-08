c4110ee26239afc1ead7bc5e738e046a01e1232a
Huacai Chen <chenhc@lemote.com>
2016-07-08 13:26:20 +0800

    MIPS: Fix r4k clockevents registration
    CPUFreq need min_delta_ticks/max_delta_ticks to be initialized, and
    this can be done by clockevents_config_and_register().
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Heiher <r@hev.cc>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/kernel/cevt-r4k.c
+++ arch/mips/kernel/cevt-r4k.c
@@ -199,19 +199,13 @@ int r4k_clockevent_init(void)
 				  CLOCK_EVT_FEAT_C3STOP |
 				  CLOCK_EVT_FEAT_PERCPU;
 
-	clockevent_set_clock(cd, mips_hpt_frequency);
-
-	/* Calculate the min / max delta */
-	cd->max_delta_ns	= clockevent_delta2ns(0x7fffffff, cd);
-	cd->min_delta_ns	= clockevent_delta2ns(0x300, cd);
-
 	cd->rating		= 300;
 	cd->irq			= irq;
 	cd->cpumask		= cpumask_of(cpu);
 	cd->set_next_event	= mips_next_event;
 	cd->event_handler	= mips_event_handler;
 
-	clockevents_register_device(cd);
+	clockevents_config_and_register(cd, mips_hpt_frequency, 0x300, 0x7fffffff);
 
 	if (cp0_timer_irq_installed)
 		return 0;
