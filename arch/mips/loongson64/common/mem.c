3f97c681ce43c4f40da9f0d58df56af2dc708646
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    Loongson: Add memory type UMA_VIDEO_RAM to pass shared VRAM info
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/loongson64/common/mem.c
+++ arch/mips/loongson64/common/mem.c
@@ -97,6 +97,14 @@ void __init prom_init_memory(void)
 				add_memory_region(loongson_memmap->map[i].mem_start,
 					0x2000, BOOT_MEM_RESERVED);
 				break;
+			case UMA_VIDEO_RAM:
+				loongson_sysconf.vram_type = VRAM_TYPE_UMA;
+				loongson_sysconf.uma_vram_addr = loongson_memmap->map[i].mem_start & 0xffffffff;
+				loongson_sysconf.uma_vram_size = loongson_memmap->map[i].mem_size;
+				add_memory_region(loongson_memmap->map[i].mem_start,
+					(u64)loongson_memmap->map[i].mem_size << 20,
+					BOOT_MEM_RESERVED);
+				break;
 			}
 		}
 	}
