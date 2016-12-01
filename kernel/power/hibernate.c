0d97bd13decc3a818670284fccf41f6b6a37ba51
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    MIPS: Loongson 3: Add CPU Autoplug support.
    CPU Autoplug is used for power saving. Loongson 3 supports CPUFreq, but
    the clock frequency of each core is always the same (cannot be adjusted
    independently). If there is a single-thread CPU-intensive program, we
    cannot use frequency scaling for power saving (if we do, performance of
    the program will slowdown). Therefore, we introduce CPU Autoplug to
    adjust the number of online cores based on the global CPU load.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Hongliang Tao <taohl@lemote.com>
    Signed-off-by: Hua Yan <yanh@lemote.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -707,6 +707,7 @@ int hibernate(void)
 	sys_sync();
 	printk("done.\n");
 
+	system_state = SYSTEM_SUSPEND_DISK;
 	error = freeze_processes();
 	if (error)
 		goto Exit;
@@ -757,6 +758,7 @@ int hibernate(void)
 			error = load_image_and_restore();
 	}
 	thaw_processes();
+	system_state = SYSTEM_RUNNING;
 
 	/* Don't bother checking whether freezer_test_done is true */
 	freezer_test_done = false;
@@ -885,6 +887,7 @@ static int software_resume(void)
 		goto Close_Finish;
 	error = load_image_and_restore();
 	thaw_processes();
+	system_state = SYSTEM_RUNNING;
  Finish:
 	__pm_notifier_call_chain(PM_POST_RESTORE, nr_calls, NULL);
 	pm_restore_console();
