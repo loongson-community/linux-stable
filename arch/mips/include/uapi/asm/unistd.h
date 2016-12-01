50396cd2e56544de8f29b69da32ddb90eafd6dea
Ralf Baechle <ralf@linux-mips.org>
2016-12-01 09:51:35 +0800

    MIPS syscall auditing patches
    this is the first cut of the MIPS auditing patches.  MIPS doesn't quite
    fit into the existing pattern of other architectures and I'd appreciate
    your comments and maybe even an Acked-by.
    
     - MIPS syscalls return a success / error flag in register $7.  If the
       flag is set then the return value in $2 is a *positive* error value.
       This means the existing AUDITSC_RESULT() macro does not work on
       MIPS and thus ptrace.c defines it's own version MIPS_AUDITSC_RESULT().
    
     - Linux on MIPS extends the traditional syscall table used by older UNIX
       implementations.  This is why 32-bit Linux syscalls are starting from
       4000; the native 64-bit syscalls start from 5000 and the N32 compat ABI
       from 6000.  The existing syscall bitmap is only large enough for at most
       2048 syscalls, so I had to increase AUDIT_BITMASK_SIZE to 256 which
       provides enough space for 8192 syscalls.  Because include/linux/audit.h
       and AUDIT_BITMASK_SIZE are exported to userspace I've used an #ifdef
       __mips__ for this.
    
     - I've introduced a flag __AUDIT_ARCH_ALT to indicate an alternative ABI.
       The name of the flag is intentionally very generic to make the name
       hopefully fit other architectures' eventual need as well.  On MIPS it
       indicates the 3rd ABI known as N32.
    
     - To make matters worse, most MIPS processors can be configured to be
       big or little endian.  Traditionally the the 64-bit little endian
       configuration is named mips64el, so I've changed references to MIPSEL64
       in audit.h to MIPS64EL.
    
     - The code treats the little endian MIPS architecture as separate from
       big endian.  Combined with the 3 ABIs that's 6 combinations.  I tried
       to sort of follow the example set by ARM which explicitly lists the
       (rare) big endian architecture variant - but it doesn't seem to very
       useful so I wonder if this could be squashed to just the three ABIs
       without consideration of endianess?
    
     - Talking about flags; I've defined the the N32 architecture flags were defined
    
        #define AUDIT_ARCH_MIPS64_N32  (EM_MIPS|__AUDIT_ARCH_ALT)
        #define AUDIT_ARCH_MIPS64EL_N32 (EM_MIPS|__AUDIT_ARCH_ALT|__AUDIT_ARCH_LE
    
        N32 is a 32-bit ABI but one that only runs on 64-bit processors as it
        uses 64-bit registers for 64-bit integers.  So I'm uncertain if the
        __AUDIT_ARCH_64BIT flags should be set or not.
    
    Thanks in advance,
    
    v2: 8b659a393171aed3dafa1d7455ac9eec1f3ed315 (kernel.org) rsp.
        25c81f4d5cec9ac8fe1f5b2072d995d0cdc9f361 (linux-mips.org) [MIPS: Split
        do_syscall_trace into two functions.] and Eric's patch [Audit: push
        audit success and retcode into arch ptrace.h] made my previous patch
        go stale and no longer applicable.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/uapi/asm/unistd.h
+++ arch/mips/include/uapi/asm/unistd.h
@@ -6,16 +6,15 @@
  *
  * Copyright (C) 1995, 96, 97, 98, 99, 2000 by Ralf Baechle
  * Copyright (C) 1999, 2000 Silicon Graphics, Inc.
- *
- * Changed system calls macros _syscall5 - _syscall7 to push args 5 to 7 onto
- * the stack. Robin Farine for ACN S.A, Copyright (C) 1996 by ACN S.A
  */
 #ifndef _UAPI_ASM_UNISTD_H
 #define _UAPI_ASM_UNISTD_H
 
 #include <asm/sgidefs.h>
 
-#if _MIPS_SIM == _MIPS_SIM_ABI32
+#if (defined(__WANT_SYSCALL_NUMBERS) &&					\
+     (__WANT_SYSCALL_NUMBERS == _MIPS_SIM_ABI32)) ||			\
+    (!defined(__WANT_SYSCALL_NUMBERS) && _MIPS_SIM == _MIPS_SIM_ABI32)
 
 /*
  * Linux o32 style syscalls are in the range from 4000 to 4999.
@@ -397,12 +396,14 @@
  */
 #define __NR_Linux_syscalls		368
 
-#endif /* _MIPS_SIM == _MIPS_SIM_ABI32 */
+#endif /* Want O32 || _MIPS_SIM == _MIPS_SIM_ABI32  */
 
 #define __NR_O32_Linux			4000
 #define __NR_O32_Linux_syscalls		368
 
-#if _MIPS_SIM == _MIPS_SIM_ABI64
+#if (defined(__WANT_SYSCALL_NUMBERS) &&					\
+	(__WANT_SYSCALL_NUMBERS == _MIPS_SIM_ABI64)) ||			\
+	(!defined(__WANT_SYSCALL_NUMBERS) && _MIPS_SIM == _MIPS_SIM_ABI64)
 
 /*
  * Linux 64-bit syscalls are in the range from 5000 to 5999.
@@ -743,12 +744,14 @@
  */
 #define __NR_Linux_syscalls		328
 
-#endif /* _MIPS_SIM == _MIPS_SIM_ABI64 */
+#endif /* Want N64 || _MIPS_SIM == _MIPS_SIM_ABI64  */
 
 #define __NR_64_Linux			5000
 #define __NR_64_Linux_syscalls		328
 
-#if _MIPS_SIM == _MIPS_SIM_NABI32
+#if (defined(__WANT_SYSCALL_NUMBERS) &&					\
+	(__WANT_SYSCALL_NUMBERS == _MIPS_SIM_NABI32)) ||		\
+	(!defined(__WANT_SYSCALL_NUMBERS) && _MIPS_SIM == _MIPS_SIM_NABI32)
 
 /*
  * Linux N32 syscalls are in the range from 6000 to 6999.
@@ -1093,7 +1096,7 @@
  */
 #define __NR_Linux_syscalls		332
 
-#endif /* _MIPS_SIM == _MIPS_SIM_NABI32 */
+#endif /* Want N32 || _MIPS_SIM == _MIPS_SIM_NABI32  */
 
 #define __NR_N32_Linux			6000
 #define __NR_N32_Linux_syscalls		332
