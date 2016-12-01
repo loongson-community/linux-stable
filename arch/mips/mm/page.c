e61304914e0819bf7913ad624253a64f3e1f698f
chenj <chenj@lemote.com>
2016-12-01 09:51:35 +0800

    Optimize copy_page

--- arch/mips/mm/page.c
+++ arch/mips/mm/page.c
@@ -46,6 +46,10 @@
 #define T1 9
 #define T2 10
 #define T3 11
+#define T4 12
+#define T5 13
+#define T6 14
+#define T7 15
 #define T9 25
 #define RA 31
 
@@ -221,6 +225,7 @@ static void set_prefetch_parameters(void)
 
 #ifdef CONFIG_CPU_LOONGSON3
 	clear_word_size = 16;
+	copy_word_size = 16;
 #endif
 
 	/*
@@ -375,20 +380,28 @@ void build_clear_page(void)
 
 static void build_copy_load(u32 **buf, int reg, int off)
 {
+#ifdef CONFIG_CPU_LOONGSON3
+	uasm_i_gslq(buf, reg, reg+4, off, A1);
+#else
 	if (cpu_has_64bit_gp_regs) {
 		uasm_i_ld(buf, reg, off, A1);
 	} else {
 		uasm_i_lw(buf, reg, off, A1);
 	}
+#endif
 }
 
 static void build_copy_store(u32 **buf, int reg, int off)
 {
+#ifdef CONFIG_CPU_LOONGSON3
+	uasm_i_gssq(buf, reg, reg+4, off, A0);
+#else
 	if (cpu_has_64bit_gp_regs) {
 		uasm_i_sd(buf, reg, off, A0);
 	} else {
 		uasm_i_sw(buf, reg, off, A0);
 	}
+#endif
 }
 
 static inline void build_copy_load_pref(u32 **buf, int off)
--- arch/mips/mm/uasm.c
+++ arch/mips/mm/uasm.c
@@ -63,7 +63,7 @@ enum opcode {
 	insn_sllv, insn_slt, insn_sltiu, insn_sltu, insn_sra, insn_srl,
 	insn_srlv, insn_subu, insn_sw, insn_sync, insn_syscall, insn_tlbp,
 	insn_tlbr, insn_tlbwi, insn_tlbwr, insn_wait, insn_wsbh, insn_xor,
-	insn_xori, insn_yield, insn_lddir, insn_ldpte, insn_gssq,
+	insn_xori, insn_yield, insn_lddir, insn_ldpte, insn_gssq, insn_gslq,
 };
 
 struct insn {
@@ -326,6 +326,7 @@ I_u2s3u1(_sc)
 I_u2s3u1(_scd)
 I_u2s3u1(_sd)
 I_u4u2u1s3(_gssq)
+I_u4u2u1s3(_gslq)
 I_u2u1u3(_sll)
 I_u3u2u1(_sllv)
 I_s3s1s2(_slt)
