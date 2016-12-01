7e8f9cebdb3bad087d79ce5438cf398727d4c982
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    MIPS: Loongson 3: Add CPU Autoplug support.
    CPU Autoplug is used for power saving. Loongson 3 supports CPUFreq, but
    the clock frequency of each core is always the same (cannot be adjusted
    independently). If there is a single-thread CPU-intensive program, we
    cannot use frequency scaling for power saving (if we do, performance of
    the program will slowdown). Therefore, we introduce CPU Autoplug to
    adjust the number of online cores based on the global CPU load.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Hongliang Tao <taohl@lemote.com>
    Signed-off-by: Hua Yan <yanh@lemote.com>

--- arch/mips/loongson64/common/pm.c
+++ arch/mips/loongson64/common/pm.c
@@ -22,6 +22,7 @@
 static unsigned int __maybe_unused cached_master_mask;	/* i8259A */
 static unsigned int __maybe_unused cached_slave_mask;
 static unsigned int __maybe_unused cached_bonito_irq_mask; /* bonito */
+static unsigned int __maybe_unused cached_autoplug_enabled;
 
 void arch_suspend_disable_irqs(void)
 {
@@ -60,8 +61,21 @@ void arch_suspend_enable_irqs(void)
 
 static int loongson_pm_begin(suspend_state_t state)
 {
+#ifdef CONFIG_LOONGSON3_CPUAUTOPLUG
+	extern int autoplug_enabled;
+	cached_autoplug_enabled = autoplug_enabled;
+	autoplug_enabled = 0;
+#endif
 	pm_set_suspend_via_firmware();
 	return 0;
+}
+
+static void loongson_pm_end(void)
+{
+#ifdef CONFIG_LOONGSON3_CPUAUTOPLUG
+	extern int autoplug_enabled;
+	autoplug_enabled = cached_autoplug_enabled;
+#endif
 }
 
 static int loongson_pm_enter(suspend_state_t state)
@@ -100,6 +114,7 @@ static const struct platform_suspend_ops loongson_pm_ops = {
 	.valid	= loongson_pm_valid_state,
 	.begin	= loongson_pm_begin,
 	.enter	= loongson_pm_enter,
+	.end	= loongson_pm_end,
 };
 
 static int __init loongson_pm_init(void)
