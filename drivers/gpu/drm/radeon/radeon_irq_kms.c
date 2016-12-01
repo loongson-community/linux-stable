f2f390bb3ba71d4abe5977fc6d25dfcdd15ca181
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    drm/radeon: recover the GPU if it fails at resume
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Binbin Zhou <zhoubb@lemote.com>

--- drivers/gpu/drm/radeon/radeon_irq_kms.c
+++ drivers/gpu/drm/radeon/radeon_irq_kms.c
@@ -270,6 +270,8 @@ static bool radeon_msi_ok(struct radeon_device *rdev)
 	return true;
 }
 
+extern void radeon_recover_callback(struct work_struct *work);
+
 /**
  * radeon_irq_kms_init - init driver interrupt info
  *
@@ -306,6 +308,9 @@ int radeon_irq_kms_init(struct radeon_device *rdev)
 	INIT_DELAYED_WORK(&rdev->hotplug_work, radeon_hotplug_work_func);
 	INIT_WORK(&rdev->dp_work, radeon_dp_work_func);
 	INIT_WORK(&rdev->audio_work, r600_audio_update_hdmi);
+
+	rdev->need_recover = 0;
+	INIT_DEFERRABLE_WORK(&rdev->recover_work, radeon_recover_callback);
 
 	rdev->irq.installed = true;
 	r = drm_irq_install(rdev->ddev, rdev->ddev->pdev->irq);
