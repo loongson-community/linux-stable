6fd2d93aa2d458b2e410e83d7b7bf6c00dce18a5
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    Staging: sb105x: Fix build and add MIPS support
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

---
+++ arch/mips/include/asm/serial.h
@@ -0,0 +1,7 @@
+#ifndef _ASM_MIPS_SERIAL_H
+#define _ASM_MIPS_SERIAL_H
+
+#define STD_COM_FLAGS (ASYNC_BOOT_AUTOCONF | ASYNC_SKIP_TEST)
+#include <asm-generic/serial.h>
+
+#endif /* _ASM_MIPS_SERIAL_H */
