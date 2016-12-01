0caba7539df02a010c517aa3af741c761445708a
Si Zhiying <sizhiying@loongson.cn>
2016-12-01 09:51:35 +0800

    Ugly workarounds for Loongson-3
    1, Remove redundant sync in llsc;
    2, Use sync+synci as __WEAK_LLSC_MB;
    3, Add sync before ll/sc and after branch target (bne).

--- arch/mips/include/asm/barrier.h
+++ arch/mips/include/asm/barrier.h
@@ -107,9 +107,13 @@
 #endif
 
 #if defined(CONFIG_WEAK_REORDERING_BEYOND_LLSC) && defined(CONFIG_SMP)
-#define __WEAK_LLSC_MB		"	sync	\n"
+# ifdef CONFIG_LOONGSON3_ENHANCEMENT
+# define __WEAK_LLSC_MB		"	.set mips64r2\n synci 0\n .set mips0\n"
+# else
+# define __WEAK_LLSC_MB		"	sync	\n"
+# endif
 #else
-#define __WEAK_LLSC_MB		"		\n"
+# define __WEAK_LLSC_MB		"		\n"
 #endif
 
 #define set_mb(var, value) \
