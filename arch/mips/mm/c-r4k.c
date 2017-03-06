16d5afb3bb56ed4a7a4475c3beb3dcfddf6f1343
Huacai Chen <chenhc@lemote.com>
2017-03-06 08:30:34 +0800

    MIPS: c-r4k: Fix Loongson-3's vcache/scache waysize calculation
    If scache.waysize is 0, r4k___flush_cache_all() will do nothing and
    then cause bugs. BTW, though vcache.waysize isn't being used by now,
    we also fix its calculation.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/mm/c-r4k.c
+++ arch/mips/mm/c-r4k.c
@@ -1152,6 +1152,7 @@ static void __cpuinit probe_vcache(void)
 	vcache_size = c->vcache.sets * c->vcache.ways * c->vcache.linesz;
 
 	c->vcache.waybit = 0;
+	c->vcache.waysize = vcache_size / c->vcache.ways;
 
 	if (system_state == SYSTEM_BOOTING)
 		pr_info("Unified victim cache %ldkB %s, linesize %d bytes.\n",
@@ -1257,6 +1258,7 @@ static void __init loongson3_sc_init(void)
 	/* Loongson-3 has 4 cores, 1MB scache for each. scaches are shared */
 	scache_size *= 4;
 	c->scache.waybit = 0;
+	c->scache.waysize = scache_size / c->scache.ways;
 	if (system_state == SYSTEM_BOOTING)
 		pr_info("Unified secondary cache %ldkB %s, linesize %d bytes.\n",
 		       scache_size >> 10, way_string[c->scache.ways], c->scache.linesz);
