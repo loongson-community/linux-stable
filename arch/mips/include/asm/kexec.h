cbb9bc197c6f038de0b5b5196376cf03a849ac4f
Huacai Chen <chenhc@lemote.com>
2018-01-26 20:13:22 +0800

    MIPS: Loongson: Add kexec/kdump support
    Cc: Eric Biederman <ebiederm@xmission.com>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/include/asm/kexec.h
+++ arch/mips/include/asm/kexec.h
@@ -36,14 +36,15 @@ static inline void crash_setup_regs(struct pt_regs *newregs,
 #ifdef CONFIG_KEXEC
 struct kimage;
 extern unsigned long kexec_args[4];
+extern const size_t relocate_new_kernel_size;
 extern int (*_machine_kexec_prepare)(struct kimage *);
 extern void (*_machine_kexec_shutdown)(void);
 extern void (*_machine_crash_shutdown)(struct pt_regs *regs);
 extern void default_machine_crash_shutdown(struct pt_regs *regs);
+extern void kexec_reboot(void);
 #ifdef CONFIG_SMP
 extern const unsigned char kexec_smp_wait[];
 extern unsigned long secondary_kexec_args[4];
-extern void (*relocated_kexec_smp_wait) (void *);
 extern atomic_t kexec_ready_to_reboot;
 extern void (*_crash_smp_send_stop)(void);
 #endif
