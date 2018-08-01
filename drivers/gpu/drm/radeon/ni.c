7226fe21a55fc03e1b9787c67a5e2782554f114d
Huacai Chen <chenhc@lemote.com>
2018-08-01 14:31:19 +0800

    drm/radeon: Reintroduce radeon_gart_restore()
    This revert commit a3eb06dbca08e3fdad7039021ae0 ("drm/radeon: Remove
    radeon_gart_restore()") and update code for the latest kernel. We do
    this because kexec/kdump need to do this, otherwise it boot fails.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- drivers/gpu/drm/radeon/ni.c
+++ drivers/gpu/drm/radeon/ni.c
@@ -1279,6 +1279,7 @@ static int cayman_pcie_gart_enable(struct radeon_device *rdev)
 	r = radeon_gart_table_vram_pin(rdev);
 	if (r)
 		return r;
+	radeon_gart_restore(rdev);
 	/* Setup TLB control */
 	WREG32(MC_VM_MX_L1_TLB_CNTL,
 	       (0xA << 7) |
